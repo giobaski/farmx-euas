c2bd466347f89ed2d61bb7711b704b3a4dcc4a8f
clyntriley <clynt.riley.legaspi.1@eek.ee>
2022-02-05 20:10:42 +0200

    added catgory

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,12 +39,14 @@ const Home = () => {
   }, []);
 
 
+  
+  
   let myLotCard = (lot) => {
-    return <div class="card  bg-secondary text-white">
-    {/* <img class="card-img-top" src="" alt="Card image cap" ></img> */}
+    return <div class="card bg-secondary text-white">
+    { }
     <div class="card-body">
       <h5 class="card-title text-info"> {lot.productName}</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
       <div class="d-flex align-items-stretch">
         <p>Available Amount: {lot.initAmount} Kg</p>
       </div>
@@ -61,6 +63,7 @@ const Home = () => {
        <input type="text" onChange={handleAmountChange} placeholder="type amount (KG)"></input>
           
       <a onClick={()=>placeOrder(lot.id)} class="btn btn-success m-lg-2">Buy</a>
+      
     </div>
   </div>
   }
@@ -79,6 +82,7 @@ const Home = () => {
                 : null
                 }
 
+           
             </header> 
             <div class="row">
                 <div class="col-lg-10">
@@ -97,11 +101,11 @@ const Home = () => {
                   <h2>Category </h2>
 
                   <ul class="list-group">
-                  <li class="list-group-item disabled">Cras justo odio</li>
-                  <li class="list-group-item">Dapibus ac facilisis in</li>
-                  <li class="list-group-item">Morbi leo risus</li>
-                  <li class="list-group-item">Porta ac consectetur ac</li>
-                  <li class="list-group-item">Vestibulum at eros</li>
+                  <li class="list-group-item">Terrace Cultivation</li>
+                  <li class="list-group-item">Mixed and Multiple Agriculture</li>
+                  <li class="list-group-item">Dry Agriculture</li>
+                  <li class="list-group-item">Plantation Agriculture </li>
+                  <li class="list-group-item">Intensive Farming </li>
                 </ul>
                   
                   
@@ -117,12 +121,13 @@ const Home = () => {
 
             <div>
            <footer class="page-footer font-small blue mt-1">
-            <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <div class="footer-copyright text-center py-3">© 2022 Copyright:
               <a href="https://farmx.ee/"> farmx.ee</a>
             </div>
             </footer>
         </div>
         </div>
+     
 
         
   );
